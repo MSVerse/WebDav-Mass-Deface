6269569fa17602064e4c50646cd6df84c8e12e40
MSVerse <135821325+MSVerse@users.noreply.github.com>
2023-06-21 14:00:33 +0700

    Update cURL.sh

--- cURL.sh
+++ cURL.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-# MIT License
-# 
-# Copyright (c) [2023] [MSVerse]
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 # Menyimpan argumen dalam variabel
 file_to_upload="$1"
 url_list="$2"
@@ -44,13 +22,9 @@ if [ ! -f "$url_list" ]; then
   exit 1
 fi
 
-# Mengunggah file ke setiap URL secara paralel
-cat "$url_list" | parallel -j 4 --timeout 10 "upload_file {}"
-
-# Fungsi untuk mengunggah file ke URL menggunakan curl
-upload_file() {
-  url="$1"
-  
+# Mengunggah file ke setiap URL
+while IFS= read -r url
+do
   # Mengunggah file ke URL menggunakan curl
   result=$(curl --max-time 10 --write-out "%{http_code}\n" --silent --output /dev/null -T "$file_to_upload" "$url")
 
@@ -61,4 +35,4 @@ upload_file() {
     echo -e "Upload gagal ke $url"
     echo ""
   fi
-}
+done < "$url_list"
